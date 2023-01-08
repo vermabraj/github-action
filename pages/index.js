8af9b8d29b350c72ff24947b2ee1900c8d3eb4ec
unknown <vermabraj>
2023-01-09 00:54:41 +0530

    aded

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+export default MyApp;
